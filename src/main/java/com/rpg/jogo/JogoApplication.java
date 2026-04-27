eebc2aa64da538c70c37baa9b6a147941f5d1fee
Janaina <janainaferreiramonteiro@hotmail.com>
2026-04-27 19:27:09 -0300

    First Commit

---
+++ src/main/java/com/rpg/jogo/JogoApplication.java
@@ -0,0 +1,13 @@
+package com.rpg.jogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogoApplication.class, args);
+	}
+
+}
