28ddcbe1889713131aa711d9d0d600497453d9ed
HebertTorres <2003htcs@gmail.com>
2026-04-30 04:41:14 -0300

    commit 4

--- src/main/java/com/rpg/jogo/Sistema/Sistema.java
+++ src/main/java/com/rpg/jogo/Sistema/Sistema.java
@@ -5,25 +5,25 @@ import java.util.Random;
 import com.rpg.jogo.Monstros.Monstro;
 
 public class Sistema {
-    int profundidade = 0;
+    private int profundidade = 0;
     public static int rolarDados20(){
         Random random = new Random();
         int dados = random.nextInt(1, 21);
         return dados;
         // Objetivo: esse método deve ser chamado por outros locais do RPG para rodar um D20
     }
-    public static boolean verificarAcerto(){
+    public boolean verificarAcerto(Personagem p1, Monstro m1){
         // aqui será necessário rolar um dado pra saber se o acerto vai ou não passar, mas para isso é necessário saber ataque e defesa dos personagens
-        Sistema.rolarDados20();
-        if (Sistema.rolarDados20() >= 10) {
-            Sistema.calcularDano();
+        int dado = Sistema.rolarDados20();
+        if (dado >= 10) {
+            calcularDano(p1, m1);
             return true;
         } else {
             System.out.println("Você errou o ataque.");
             return false;
         }
     }
-    public static double calcularDano(Personagem p1, Monstro m1){
+    public double calcularDano(Personagem p1, Monstro m1){
         double dano;
         if (p1.forca <= m1.defesa) {
             dano = 1;
@@ -31,24 +31,22 @@ public class Sistema {
             dano = (p1.forca - m1.defesa);
         }
         return dano;
-        // A gente ficou um pouco travado de fazer esse, por depender da classe personagem
         // usamos objetos hipoteticos. Objetivo: se a defesa do mob for maior do que a força do personagem, ele deve dar ao menos 1 de dano, recompensando o acerto do dado.
     }
-    public static boolean resultadoDungeon(Monstro m1, int profudidade){
+    public boolean resultadoDungeon(Monstro m1){
         if (m1.vida <= 0) {
+            aumentarProfundidade();
             return true;
-            Sistema.aumentarProfundidade(profudidade);
         } else {
             return false;
         }
-        // provavelmente a lógica do return está errado
         // se vitoria acontecer, a profundidade aumenta. se vitoria é false, e esse método for chamado, então a dungeon será contabilizada como fail.
     }
-    public int getProfundidade(int profudidade){
-        return profudidade;
+    public void mostrarProfundidade(){
+        System.out.println("A profundidade é " + profundidade);
     }
-    public static void aumentarProfundidade(int profudidade){
-        profudidade++;
-        // se esse método for chamado, a profudidade aumentará em +1, provavelmente a variavel profundidade não seria criado aqui, mas numa aba "dungeon"
+    public void aumentarProfundidade(){
+        this.profundidade++;
+        // se esse método for chamado, a profudidade aumentará em +1
     }
 }
